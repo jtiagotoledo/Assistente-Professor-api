a9399826cd09deb02d50c7e645177191fe4d1243
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2025-05-09 19:16:12 -0300

    melhorado controller v2

--- controllers/professoresController.js
+++ controllers/professoresController.js
@@ -1,18 +1,26 @@
 const db = require('../config/db');
-const generateUUID = require('../utils/uuid'); // Função que gera UUID
+const generateUUID = require('../utils/uuid'); // Importar a função para gerar UUID
 
+// Função para obter todos os professores
+exports.getAll = (req, res) => {
+  db.query('SELECT * FROM professores', (err, results) => {
+    if (err) {
+      return res.status(500).json({ erro: err.message });
+    }
+    res.json(results);
+  });
+};
+
+// Função para criar um novo professor
 exports.create = (req, res) => {
   const { nome, email } = req.body;
 
-  // Validação básica
   if (!nome || !email) {
     return res.status(400).json({ erro: 'Nome e email são obrigatórios.' });
   }
 
-  // Gerar UUID para o novo professor
-  const id = generateUUID(); 
+  const id = generateUUID(); // Gerar UUID para o novo professor
 
-  // Inserir no banco de dados
   db.query(
     'INSERT INTO professores (id, nome, email) VALUES (?, ?, ?)',
     [id, nome, email],
