626caffb19cc1efd9804811760f8083ce6f6c746
jtiagotoledo <jtiagotoledo@gmail.com>
2025-05-15 19:25:32 -0300

    criado índices da tabela

--- schema.sql
+++ schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE IF NOT EXISTS acessos (
     id_professor CHAR(36) NOT NULL,
     email_professor VARCHAR(255) NOT NULL,
     data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_professor) REFERENCES professores(id)
+    FOREIGN KEY (id_professor) REFERENCES professores(id),
+    INDEX idx_acessos_id_professor (id_professor)
 );
 
 -- Períodos
@@ -22,7 +23,8 @@ CREATE TABLE IF NOT EXISTS periodos (
     id CHAR(36) PRIMARY KEY,
     nome VARCHAR(100),
     id_professor CHAR(36),
-    FOREIGN KEY (id_professor) REFERENCES professores(id)
+    FOREIGN KEY (id_professor) REFERENCES professores(id),
+    INDEX idx_periodos_id_professor (id_professor)
 );
 
 -- Classes
@@ -30,19 +32,22 @@ CREATE TABLE IF NOT EXISTS classes (
     id CHAR(36) PRIMARY KEY,
     nome VARCHAR(100),
     id_periodo CHAR(36),
-    FOREIGN KEY (id_periodo) REFERENCES periodos(id)
+    FOREIGN KEY (id_periodo) REFERENCES periodos(id),
+    INDEX idx_classes_id_periodo (id_periodo)
 );
 
 -- Alunos
 CREATE TABLE IF NOT EXISTS alunos (
     id CHAR(36) PRIMARY KEY,             
-    numero VARCHAR(100) NOT NULL,                      
-    nome VARCHAR(100) NOT NULL,               
+    numero INT NOT NULL,                      
+    nome VARCHAR(100) NOT NULL,                
     inativo BOOLEAN DEFAULT FALSE,            
     media_notas DECIMAL(5,2),                  
     porc_frequencia DECIMAL(5,2),       
     id_classe CHAR(36),                        
-    FOREIGN KEY (id_classe) REFERENCES classes(id)
+    FOREIGN KEY (id_classe) REFERENCES classes(id),
+    INDEX idx_alunos_id_classe (id_classe),
+    INDEX idx_alunos_numero (numero)
 );
 
 -- Frequências
@@ -52,7 +57,9 @@ CREATE TABLE IF NOT EXISTS frequencias (
     atividades VARCHAR(100),
     presente BOOLEAN,
     id_aluno CHAR(36),
-    FOREIGN KEY (id_aluno) REFERENCES alunos(id)
+    FOREIGN KEY (id_aluno) REFERENCES alunos(id),
+    INDEX idx_frequencias_id_aluno (id_aluno),
+    INDEX idx_frequencias_data (data)
 );
 
 -- Notas
@@ -62,5 +69,7 @@ CREATE TABLE IF NOT EXISTS notas (
     titulo VARCHAR(100),
     valor DECIMAL(5,2),
     id_aluno CHAR(36),
-    FOREIGN KEY (id_aluno) REFERENCES alunos(id)
+    FOREIGN KEY (id_aluno) REFERENCES alunos(id),
+    INDEX idx_notas_id_aluno (id_aluno),
+    INDEX idx_notas_data (data)
 );
