6d5de94b5a0467a666509230bf6909186dd8b76b
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2025-05-23 18:24:14 -0300

    teste de contagem frequencia v4

--- routes/frequenciasRoutes.js
+++ routes/frequenciasRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/frequenciasController');
 
+router.get('/porcentagem', controller.getPorcentagemFrequencia);
+router.get('/classe/:id_classe/data/:data', controller.getFrequenciasPorClasseEData);
 router.post('/', controller.create);
 router.get('/:id_data_frequencia', controller.getByDataFrequencia);
 router.put('/:id', controller.update);
-router.get('/porcentagem', controller.getPorcentagemFrequencia);
-router.get('/classe/:id_classe/data/:data', controller.getFrequenciasPorClasseEData);
 
 module.exports = router;
