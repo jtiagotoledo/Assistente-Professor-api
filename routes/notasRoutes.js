00fe00c4654788666ca130afeb9d5aae3335deb3
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2025-05-23 19:29:04 -0300

    teste de contagem frequencia v8

--- routes/notasRoutes.js
+++ routes/notasRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/notasController');
 
+router.get('/classe/:id_classe/todas', controller.getNotasPorClasse);
 router.post('/', controller.create);
 router.get('/:id_data_nota', controller.getByDataNota);
 router.put('/:id', controller.update);
