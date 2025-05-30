8e4e84cdc3e97e2cd0638327974d2a5926dfa195
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2025-05-30 19:52:11 -0300

    implementação do jwt v5

--- routes/professoresRoutes.js
+++ routes/professoresRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middlewares/authmiddleware');
 const controller = require('../controllers/professoresController');
 
 // Rota para pegar os dados do professor autenticado
