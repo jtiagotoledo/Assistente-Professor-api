a7639503f502f1c9935df3383448696f8fab8959
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2025-05-30 19:16:10 -0300

    implementação do jwt v2

--- routes/professoresRoutes.js
+++ routes/professoresRoutes.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/professoresController');
 
 router.get('/', controller.getAll);
 router.post('/', controller.create);
-router.get('/uuid/:uuid', controller.getByUUID);
+router.get('/id/:id', controller.getById);
 router.delete('/:id', controller.delete);
 
 module.exports = router;
