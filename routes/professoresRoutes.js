fe755d0cf9d7c7e1a5a72809acd23e34032ef1dc
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2025-05-30 19:42:44 -0300

    implementação do jwt v3

--- routes/professoresRoutes.js
+++ routes/professoresRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
 const controller = require('../controllers/professoresController');
 
-router.get('/', controller.getAll);
-router.post('/', controller.create);
-router.get('/id/:id', controller.getById);
-router.delete('/:id', controller.delete);
+// Rota para pegar os dados do professor autenticado
+router.get('/me', authMiddleware, (req, res) => {
+  // Aqui você pode retornar as infos que tem no token (req.user)
+  // Ou buscar no banco se quiser mais dados
+  res.json({ message: 'Autorizado', user: req.user });
+});
+
+// Rotas protegidas (exigem token)
+router.get('/', authMiddleware, controller.getAll);
+router.post('/', authMiddleware, controller.create);
+router.get('/id/:id', authMiddleware, controller.getById);
+router.delete('/:id', authMiddleware, controller.delete);
 
 module.exports = router;
