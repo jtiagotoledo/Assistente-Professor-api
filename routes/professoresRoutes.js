c7584dd5938cf730867421b8b0cb1ef886d15875
jtiagotoledo <jtiagotoledo@gmail.com>
2025-06-01 13:04:19 -0300

    adicionado authMiddleware em todas as rotas v2

--- routes/professoresRoutes.js
+++ routes/professoresRoutes.js
@@ -3,16 +3,12 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/authmiddleware');
 const controller = require('../controllers/professoresController');
 
-// Rota para pegar os dados do professor autenticado
 router.get('/me', authMiddleware, (req, res) => {
-  // Aqui você pode retornar as infos que tem no token (req.user)
-  // Ou buscar no banco se quiser mais dados
   res.json({ message: 'Autorizado', user: req.user });
 });
 
-// Rotas protegidas (exigem token)
 router.get('/', authMiddleware, controller.getAll);
-router.post('/', authMiddleware, controller.create);
+router.post('/', controller.create);
 router.get('/id/:id', authMiddleware, controller.getById);
 router.delete('/:id', authMiddleware, controller.delete);
 
