e7c650960e55460962ca5f591a492baf4df1cebe
Tiago Toledo <jtiagotoledo@gmail.com>
2025-10-07 15:18:31 -0300

    rota para trocar senha v5

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/authController');
 router.post('/login', authController.login);       
 router.post('/refresh', authController.refreshToken);  
 router.post('/google', authController.googleLogin); 
+router.get('/redefinir-senha', authController.redirecionarRedefinicaoSenha);
 router.post('/esqueci-senha', authController.esqueciSenha);   
 
 module.exports = router;
